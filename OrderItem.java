7f6580c4520bff67ca66aa4d233f210bcb8dbe2b
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:25:47 +0300

    Update OrderItem.java

--- OrderItem.java
+++ OrderItem.java
@@ -2,9 +2,4 @@ public class OrderItem {
 
     private int quantity;
     private double price;
-
-    public OrderItem(int quantity, double price) {
-        this.quantity = quantity;
-        this.price = price;
-    }
 }
