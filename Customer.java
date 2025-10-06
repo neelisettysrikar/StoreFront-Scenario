670c3e378904d04fc3b17310f33fb4e88d2d45d0
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:24:00 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,12 +1,10 @@
 public class Customer {
 
     public void browseProducts() {
-        // TODO - implement Customer.browseProducts
         throw new UnsupportedOperationException();
     }
 
     public void placeOrder() {
-        // TODO - implement Customer.placeOrder
         throw new UnsupportedOperationException();
     }
 }
