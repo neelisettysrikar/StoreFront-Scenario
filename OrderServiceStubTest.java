ae0dd00e1cf8e9f9a4e83cd34bbd2a57f505f63b
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 10:21:15 +0300

    Update OrderServiceStubTest.java

--- OrderServiceStubTest.java
+++ OrderServiceStubTest.java
@@ -1,34 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-// Stub payment processor that always succeeds
 class PaymentProcessorStub {
     boolean process(float amount) {
-        return true;
-    }
-}
-
-class OrderService {
-    private PaymentProcessorStub paymentProcessor;
-
-    public OrderService(PaymentProcessorStub paymentProcessor) {
-        this.paymentProcessor = paymentProcessor;
-    }
-
-    public boolean placeOrder(Order order) {
-        return paymentProcessor.process(order.getTotalAmount());
+        return true; // always succeeds
     }
 }
 
 public class OrderServiceStubTest {
 
     @Test
-    void testOrderProcessedWithStub() {
+    void testStubAlwaysSucceeds() {
         PaymentProcessorStub stub = new PaymentProcessorStub();
-        OrderService service = new OrderService(stub);
-        Order order = new Order(1, 500f);
+        boolean result = stub.process(200f);
 
-        boolean result = service.placeOrder(order);
         assertTrue(result, "Stub should always return true");
     }
 }
