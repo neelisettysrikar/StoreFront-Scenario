4bbb3b05b25ad930c6f16592803fb17026600234
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:26:11 +0300

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -5,15 +5,7 @@ public class Payment {
     private float amount;
     private String status;
 
-    public Payment(int paymentID, String method, float amount, String status) {
-        this.paymentID = paymentID;
-        this.method = method;
-        this.amount = amount;
-        this.status = status;
-    }
-
     public void processPayment() {
-        // TODO - implement Payment.processPayment
         throw new UnsupportedOperationException();
     }
 }
