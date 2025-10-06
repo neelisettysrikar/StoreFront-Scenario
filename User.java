70824ba739918ef02c08c913f877911483f17c61
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:27:32 +0300

    Update User.java

--- User.java
+++ User.java
@@ -5,25 +5,15 @@ public class User {
     private String email;
     private String password;
 
-    public User(int userID, String name, String email, String password) {
-        this.userID = userID;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
     public void login() {
-        // TODO - implement User.login
         throw new UnsupportedOperationException();
     }
 
     public void logout() {
-        // TODO - implement User.logout
         throw new UnsupportedOperationException();
     }
 
     public void register() {
-        // TODO - implement User.register
         throw new UnsupportedOperationException();
     }
 }
