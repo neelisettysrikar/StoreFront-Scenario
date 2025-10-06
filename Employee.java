d0ff71fd9fa0420c90921c2418c4b2735f93696c
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:24:22 +0300

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,12 +1,10 @@
 public class Employee {
 
     public void updateInventory() {
-        // TODO - implement Employee.updateInventory
         throw new UnsupportedOperationException();
     }
 
     public void assistCustomers() {
-        // TODO - implement Employee.assistCustomers
         throw new UnsupportedOperationException();
     }
 }
