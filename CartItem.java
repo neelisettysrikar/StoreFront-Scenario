47f672102e003e43f88a1007cc59baa7a578f915
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:23:01 +0300

    Update CartItem.java

--- CartItem.java
+++ CartItem.java
@@ -2,9 +2,4 @@ public class CartItem {
 
     private int quantity;
     private double subtotal;
-
-    public CartItem(int quantity, double subtotal) {
-        this.quantity = quantity;
-        this.subtotal = subtotal;
-    }
 }
