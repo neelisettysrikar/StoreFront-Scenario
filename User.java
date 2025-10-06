f0b1d1104f2142c635821408a864beaffee5a86f
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 10:33:52 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,23 +1,29 @@
 public class User {
 
-	private int userID;
-	private str name;
-	private str email;
-	private str password;
+    private int userID;
+    private String name;
+    private String email;
+    private String password;
 
-	public void login() {
-		// TODO - implement User.login
-		throw new UnsupportedOperationException();
-	}
+    public User(int userID, String name, String email, String password) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-	public void logout() {
-		// TODO - implement User.logout
-		throw new UnsupportedOperationException();
-	}
+    public void login() {
+        // TODO - implement User.login
+        throw new UnsupportedOperationException();
+    }
 
-	public void register() {
-		// TODO - implement User.register
-		throw new UnsupportedOperationException();
-	}
+    public void logout() {
+        // TODO - implement User.logout
+        throw new UnsupportedOperationException();
+    }
 
+    public void register() {
+        // TODO - implement User.register
+        throw new UnsupportedOperationException();
+    }
 }
