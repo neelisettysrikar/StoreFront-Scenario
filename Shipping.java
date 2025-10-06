27d64dd35b136786ec83fbf7d52e7db34b0766c5
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 10:32:51 +0300

    Update Shipping.java

--- Shipping.java
+++ Shipping.java
@@ -1,13 +1,19 @@
 public class Shipping {
 
-	private int shipingID;
-	private str method;
-	private double cost;
-	private str status;
+    private int shippingID;
+    private String method;
+    private double cost;
+    private String status;
 
-	public void processPayment() {
-		// TODO - implement Shipping.processPayment
-		throw new UnsupportedOperationException();
-	}
+    public Shipping(int shippingID, String method, double cost, String status) {
+        this.shippingID = shippingID;
+        this.method = method;
+        this.cost = cost;
+        this.status = status;
+    }
 
+    public void processShipping() {
+        // TODO - implement Shipping.processShipping
+        throw new UnsupportedOperationException();
+    }
 }
