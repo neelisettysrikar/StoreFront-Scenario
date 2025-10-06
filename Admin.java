b5c1b9d4866e95a9a8a9b35e9f09b7eaafef70e3
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:22:11 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,14 +2,6 @@ public class Admin {
 
     private String role;
 
-    public Admin(String role) {
-        this.role = role;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
     public void addProduct() {
         // TODO - implement Admin.addProduct
         throw new UnsupportedOperationException();
