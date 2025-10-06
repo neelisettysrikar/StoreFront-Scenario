16c690e397659bde32aa46964316d074622e5acd
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 09:53:32 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,18 +1,32 @@
+import java.time.LocalDateTime;
+
 public class Order {
 
-	private int orderID;
-	private datetime date;
-	private str status;
-	private float totalAmount;
+    private int orderID;
+    private LocalDateTime date;
+    private String status;
+    private float totalAmount;
 
-	public void confirmOrder() {
-		// TODO - implement Order.confirmOrder
-		throw new UnsupportedOperationException();
-	}
+    public Order(int orderID, float totalAmount) {
+        this.orderID = orderID;
+        this.totalAmount = totalAmount;
+        this.date = LocalDateTime.now();
+        this.status = "Pending";
+    }
 
-	public void cancelOrder() {
-		// TODO - implement Order.cancelOrder
-		throw new UnsupportedOperationException();
-	}
+    public void confirmOrder() {
+        this.status = "Confirmed";
+    }
 
+    public void cancelOrder() {
+        this.status = "Cancelled";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public float getTotalAmount() {
+        return totalAmount;
+    }
 }
