1676a5047ae304bf3d5e3d7a8ffa0c892d39a85e
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:27:10 +0300

    Update Store.java

--- Store.java
+++ Store.java
@@ -4,11 +4,4 @@ public class Store {
     private String name;
     private String location;
     private String openingHours;
-
-    public Store(int storeID, String name, String location, String openingHours) {
-        this.storeID = storeID;
-        this.name = name;
-        this.location = location;
-        this.openingHours = openingHours;
-    }
 }
