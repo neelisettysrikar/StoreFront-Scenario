41daae2eb0c6d64246aa70496749d4b4772f3a2c
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 09:52:40 +0300

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,21 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
 
-	private int cartID;
-	private float totalPrice;
+    private int cartID;
+    private float totalPrice;
+    private List<Product> items = new ArrayList<>();
 
-	public void addItem() {
-		// TODO - implement Cart.addItem
-		throw new UnsupportedOperationException();
-	}
+    public void addItem(Product product) {
+        items.add(product);
+        calculateTotal();
+    }
 
-	public void removeItem() {
-		// TODO - implement Cart.removeItem
-		throw new UnsupportedOperationException();
-	}
+    public void removeItem(Product product) {
+        items.remove(product);
+        calculateTotal();
+    }
 
-	public void calculateTotal() {
-		// TODO - implement Cart.calculateTotal
-		throw new UnsupportedOperationException();
-	}
+    public void calculateTotal() {
+        totalPrice = 0;
+        for (Product p : items) {
+            totalPrice += p.getPrice();
+        }
+    }
 
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public int getItemCount() {
+        return items.size();
+    }
 }
