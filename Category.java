818d33bdbc374d7f09ec9a572d5c6f903ff04089
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 12:23:31 +0300

    Update Category.java

--- Category.java
+++ Category.java
@@ -2,9 +2,4 @@ public class Category {
 
     private int categoryID;
     private String name;
-
-    public Category(int categoryID, String name) {
-        this.categoryID = categoryID;
-        this.name = name;
-    }
 }
