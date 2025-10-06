99719c48de618290c06989f13dd1deb7ed054a1e
neelisettysrikar <neelisettysrikar@gmail.com>
2025-10-06 10:32:31 +0300

    Update Promotion.java

--- Promotion.java
+++ Promotion.java
@@ -1,14 +1,23 @@
+import java.time.LocalDate;
+
 public class Promotion {
 
-	private int promoID;
-	private string description;
-	private double discountPercentage;
-	private date startDate;
-	private date endDate;
+    private int promoID;
+    private String description;
+    private double discountPercentage;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
-	public void applyPromotion() {
-		// TODO - implement Promotion.applyPromotion
-		throw new UnsupportedOperationException();
-	}
+    public Promotion(int promoID, String description, double discountPercentage, LocalDate startDate, LocalDate endDate) {
+        this.promoID = promoID;
+        this.description = description;
+        this.discountPercentage = discountPercentage;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 
+    public void applyPromotion() {
+        // TODO - implement Promotion.applyPromotion
+        throw new UnsupportedOperationException();
+    }
 }
